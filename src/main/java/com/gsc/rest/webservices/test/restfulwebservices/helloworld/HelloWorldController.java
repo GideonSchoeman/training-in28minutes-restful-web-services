387c26a06809cf2bd70ba304b2f5d8a9d7cc0529
gideon.schoeman <gideon.schoeman@gmail.com>
2018-06-28 18:50:18 +0200

    After HATEOAS, Swagger and Internationilation.

--- src/main/java/com/gsc/rest/webservices/test/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/gsc/rest/webservices/test/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,12 +1,20 @@
 package com.gsc.rest.webservices.test.restfulwebservices.helloworld;
 
-import com.gsc.rest.webservices.test.restfulwebservices.helloworld.HelloWorldBean;
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private MessageSource messageSource; 
 
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
@@ -22,5 +30,11 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
         return new HelloWorldBean(String.format("Hello World, %s", name));
     }
+
+    @GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		return messageSource.getMessage("good.morning.message", null, 
+									LocaleContextHolder.getLocale());
+	}
 
 }
